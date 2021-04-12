7eaf95b4105f14de5cd81d9a22ab620a5d62227e
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-12 13:20:46 -0300

    exercicio do Caio vira desafio, faz exercicio com a ideia do Fernando

---
+++ dados/partidas_brasileirao.R
@@ -0,0 +1,26 @@
+# Prepara a base para os exercicios de funcoes
+# remotes::install_github("williamorim/brasileirao")
+
+brasileirao::matches %>%
+  dplyr::mutate(home = brasileirao::fix_names(home),
+                away = brasileirao::fix_names(away)) %>%
+  dplyr::rename("temporada" = season,
+                "data" = date,
+                "gols" = score,
+                "time_casa" = home,
+                "time_visitante" = away) %>%
+  tidyr::separate(
+    gols,
+    into = c("gols_casa", "gols_visitante"),
+    sep = "x",
+    remove = FALSE
+  ) %>%
+  dplyr::relocate(gols_casa, gols_visitante,  .after = time_visitante) %>%
+  dplyr::mutate(
+    quem_ganhou = dplyr::case_when(
+      gols_casa > gols_visitante ~ time_casa,
+      gols_visitante > gols_casa ~ time_visitante,
+      gols_visitante == gols_casa ~ "Empate"
+    )
+  ) %>% 
+  readr::write_csv2("dados/partidas_brasileirao.csv")
--- exercicios/exercicio-funcao.R
+++ exercicios/exercicio-funcao.R
@@ -1,11 +1,65 @@
-library(tidyverse)
-library(xml2)
+# Melhor time da temporada------------------------------------------------------
+# Transforme o código abaixo em uma função.
+# Objetivo: função que retorna qual é o time com mais vitórias na temporada.
+# A função deve ser chamada melhor_time_temporada()
+# e ter o argumento ano.
 
-nm <- c("nome", "desc", "peso", "valor", "add")
-"https://www.beleaf.com.br/cardapio-marmitas-veganas" %>% 
-  read_html() %>% 
-  xml_find_all("//div[contains(@class, 'product-item-details')]") %>% 
-  map(~str_squish(xml_text(xml_children(.x)))) %>% 
-  map_dfr(~enframe(set_names(.x, nm)), .id = "item") %>% 
-  pivot_wider() %>% 
-  select(item, nome, desc, peso, valor)
+library(tidyverse)
+
+ano <- 2018
+
+"dados/partidas_brasileirao.csv" %>% 
+read_csv2() %>%
+  group_by(temporada) %>%
+  filter(quem_ganhou != "Empate", temporada %in% ano) %>%
+  count(quem_ganhou, sort = TRUE, name = "n_vitorias") %>%
+  filter(n_vitorias == max(n_vitorias)) %>%
+  arrange(temporada)
+
+
+# Experimente se a função está funcionando:
+
+# Use a função para ver se ela funciona!
+# Qual é o time que ganhou mais jogos na temporada de 2020?
+# O resultado esperado é o time Flamengo!
+
+melhor_time_temporada(ano = 2020)
+
+
+# Experimente a função criada usando os anos de 2019 e 2020.
+# O resultado esperado é o time Flamengo (para os dois anos)!
+
+melhor_time_temporada(ano = c(2019, 2020))
+
+# Desafio! Adapte a função para que o valor padrão
+# para o argumento `anos` sejam todos os anos da base,
+# e o código abaixo funcione!
+
+melhor_time_temporada()
+
+# Ano em que o time teve menos vitórias ----------------------------------------
+# Transforme o código abaixo em uma função.
+# Objetivo: função que retorna qual é a temporada em que o time teve menos vitórias
+# A função deve ser chamada encontrar_pior_ano_time()
+# e ter o argumento time.
+
+library(tidyverse)
+time <- "Cruzeiro"
+
+"dados/partidas_brasileirao.csv" %>% 
+  read_csv2() %>%
+  group_by(temporada, quem_ganhou) %>%
+  filter(quem_ganhou != "Empate", quem_ganhou %in% time) %>%
+  count(quem_ganhou, sort = TRUE, name = "n_vitorias") %>%
+  ungroup() %>%
+  filter(n_vitorias == min(n_vitorias))
+
+
+# Experimente se a função está funcionando:
+
+# Use a função para ver se ela funciona!
+# Qual é a temporada em que o Flamengo ganhou menos partidas? 
+# O resultado esperado é 2010
+
+
+encontrar_pior_ano_time(time = "Flamengo")
