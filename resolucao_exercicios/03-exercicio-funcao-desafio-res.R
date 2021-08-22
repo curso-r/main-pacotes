acdef47ee48529d51872c0051b388b9304af0783
Beatriz Milz <beatriz.milz@hotmail.com>
2021-08-22 10:18:50 -0300

    arruma a resolucao do exercicio pois o site mudou e é com ws

--- resolucao_exercicios/03-exercicio-funcao-desafio-res.R
+++ resolucao_exercicios/03-exercicio-funcao-desafio-res.R
@@ -11,12 +11,12 @@
 #' @examples
 #' buscar_marmitas_veganas()
 buscar_marmitas_veganas <- function() {
-  nm <- c("nome", "desc", "peso", "valor", "add")
+  nm <- c("nome", "desc", "valor")
   "https://www.beleaf.com.br/cardapio-marmitas-veganas" %>%
     xml2::read_html() %>%
     xml2::xml_find_all("//div[contains(@class, 'product-item-details')]") %>%
     purrr::map(~ stringr::str_squish(xml2::xml_text(xml2::xml_children(.x)))) %>%
     purrr::map_dfr(~ tibble::enframe(purrr::set_names(.x, nm)), .id = "item") %>%
     tidyr::pivot_wider() %>%
-    dplyr::select(item, nome, desc, peso, valor)
+    dplyr::select(item, nome, desc, valor)
 }
