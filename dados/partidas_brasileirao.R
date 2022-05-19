ed28d56ff027b6fe3f246b813ecf5a1b3eb82acc
Beatriz Milz <milz.bea@gmail.com>
2022-05-19 11:05:35 -0300

    atualiza dados das partidas do brasileirao

--- dados/partidas_brasileirao.R
+++ dados/partidas_brasileirao.R
@@ -5,7 +5,7 @@ brasileirao::matches %>%
   dplyr::mutate(
     home = brasileirao::fix_names(home),
     away = brasileirao::fix_names(away)
-  ) %>%
+  ) %>% 
   dplyr::rename(
     "temporada" = season,
     "data" = date,
@@ -18,13 +18,14 @@ brasileirao::matches %>%
     into = c("gols_casa", "gols_visitante"),
     sep = "x",
     remove = FALSE
-  ) %>%
+  ) %>% 
   dplyr::relocate(gols_casa, gols_visitante, .after = time_visitante) %>%
   dplyr::mutate(
     quem_ganhou = dplyr::case_when(
+      gols_casa == "" ~ "Jogo futuro",
       gols_casa > gols_visitante ~ time_casa,
       gols_visitante > gols_casa ~ time_visitante,
       gols_visitante == gols_casa ~ "Empate"
     )
-  ) %>%
+  ) %>%View()
   readr::write_csv2("dados/partidas_brasileirao.csv")
